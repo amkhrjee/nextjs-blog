be008a0c83aeb04597d83b80a9fa06b2c7018dc7
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2021-10-30 15:12:17 +0530

    added markdown blogs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostData = getSortedPostData();
+  return {
+    props: {
+      allPostData,
+    },
+  };
+}
+
+export default function Home({ allPostData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +24,20 @@ export default function Home() {
           You can follow me on twitter{" "}
           <a href="https://www.twitter.com/its_aniruddh">@its_aniruddh</a>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
